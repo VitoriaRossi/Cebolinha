a8157f9ca6f4db22718921a28cb28b3b56f20903
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-12-09 14:37:08 -0300

    Realização da atividade sobre Replace.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string fraseDigitada, fraseComDislalia;
+Console.WriteLine("Digite uma flase: ");
+fraseDigitada = Console.ReadLine();
+
+fraseComDislalia = fraseDigitada
+.Replace("r", "l")
+.Replace("R", "L");
+
+Console.WriteLine(fraseComDislalia);
